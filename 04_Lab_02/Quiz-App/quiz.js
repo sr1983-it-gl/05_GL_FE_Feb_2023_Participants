1eb873940cbb5b157d2903c9289910c1bba01580
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-09-24 11:06:09 +0530

    Support for Answer and Question Objects

--- 04_Lab_02/Quiz-App/quiz.js
+++ 04_Lab_02/Quiz-App/quiz.js
@@ -18,3 +18,26 @@ const answerHTML = new Answer("HTML");
 
 const question1 = new Question(1, "Javascript Support", [answerFunctions, answerXHTML, answerCSS, answerHTML], answerFunctions)
 
+const answerJQuery = new Answer("JQuery")
+const answerXML = new Answer("XML");
+
+const question2 = new Question(2, "Which language is used for styling web pages?", [answerHTML, answerJQuery, answerCSS, answerXML], answerCSS)
+
+const answerPythonScript = new Answer("Python Script");
+
+const answerDjango = new Answer("Django");
+const answerNodeJS = new Answer("Node JS");
+
+const question3 = new Question(3, "Which is not a Javascript framework?", [answerPythonScript, answerJQuery, answerDjango, answerNodeJS], answerPythonScript);
+
+const answerPHP = new Answer("PHP");
+const answerJS = new Answer("JS");
+const answerAll = new Answer("All");
+
+const question4 = new Question(4, "Which is used to connect to Database?", [answerPHP, answerHTML, answerJS, answerAll], answerPHP);
+
+const answerLanguage = new Answer("Language");
+const answerProgrammingLanguage = new Answer("Programming Language");
+const answerDevelopment = new Answer("Development");
+
+const question5 = new Question(5, "Java Script is a ", [answerLanguage, answerProgrammingLanguage, answerDevelopment, answerAll], answerProgrammingLanguage)
