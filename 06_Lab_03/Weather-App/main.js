494f602ac45c0d61a5c7d88a2aead0ff6155072a
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-10-08 12:33:30 +0530

    Weather App - HTML Integration with JS

---
+++ 06_Lab_03/Weather-App/main.js
@@ -0,0 +1,7 @@
+
+
+import {WeatherApp} from "./weather-app/weather-app.js";
+
+
+// create obj for weather-app
+// init()
---
+++ 06_Lab_03/Weather-App/weather-app/weather-app.js.js
@@ -0,0 +1,26 @@
+
+
+class WeatherApp {
+
+  init(){
+
+    this.addListeners();
+  }
+
+  addListeners(){
+
+    // key-press
+    // keypress
+      // Check for Enter Key []
+        // event.keyCode == 13
+
+        // object for weather-api
+        // construct-url
+        // invoke-url
+        // response-mapper
+
+      // update the HTML elements
+  }
+}
+
+export {WeatherApp}
