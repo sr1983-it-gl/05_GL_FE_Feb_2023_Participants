2b32f406df2da71bf5c93a965f15403d9bce75e0
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-10-08 12:53:57 +0530

    Weather App - HTML Integration with JS

--- 06_Lab_03/Weather-App/main.js
+++ 06_Lab_03/Weather-App/main.js
@@ -2,6 +2,8 @@
 
 import {WeatherApp} from "./weather-app/weather-app.js";
 
+const weatherApp = new WeatherApp();
+weatherApp.init();
 
 // create obj for weather-app
 // init()
--- 06_Lab_03/Weather-App/weather-app/weather-app.js
+++ 06_Lab_03/Weather-App/weather-app/weather-app.js
@@ -1,4 +1,6 @@
 
+import {WeatherAPI} from "./weather-api.js"
+import { WeatherUtils } from "./weather-utils.js";
 
 class WeatherApp {
 
@@ -9,13 +11,60 @@ class WeatherApp {
 
   addListeners(){
 
+    const searchTextFieldElement = document.querySelector(".search-box")
+
+    searchTextFieldElement.addEventListener("keypress", (event) => {
+
+      const keyCode = event.keyCode;
+      
+      console.log(`Key Code ${keyCode}`);
+
+      if (keyCode === 13){
+        // Enter Key
+
+        const userInput = event.target.value
+
+        const weatherAPIObj = new WeatherAPI();
+        weatherAPIObj.constructURL(userInput);
+        
+        weatherAPIObj.invokeURL2()
+          .then( (responseJSON) => {
+
+            const convertedResponse 
+              = WeatherUtils.convertResponse(responseJSON);
+            
+            // Location
+            const locationElement = document.querySelector(".location .city")
+            locationElement.innerText = convertedResponse.LOCATION;
+
+            // Date
+            const dateElement = document.querySelector(".location .date")
+            dateElement.innerText = convertedResponse.DATE;
+
+            // Temperature
+            const temperatureElement = document.querySelector(".current .temp")
+            temperatureElement.innerText = convertedResponse.TEMPERATURE;
+
+            // Temperature Type
+            const temperatureTypeElement = document.querySelector(".current .weather")
+            temperatureTypeElement.innerText = convertedResponse.TEMPERATURE_TYPE;
+
+            // Low-High Element
+            const lowHighElement = document.querySelector(".current .hi-low")
+            lowHighElement.innerText 
+              = `${convertedResponse.MIN_TEMPERATURE} / ${convertedResponse.MAX_TEMPERATURE}`;
+
+          });
+      }
+    })
+
     // key-press
     // keypress
       // Check for Enter Key []
         // event.keyCode == 13
 
         // object for weather-api
-        // construct-url
+        // construct-url(user-input)
         // invoke-url
         // response-mapper
 
