7dd1d4059e3aa25c57dc0402a749e2942bda7041
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-10-08 11:06:14 +0530

    Weather App - Construct URL - Implementation

--- 06_Lab_03/Weather-App/weather-app/weather-api.js
+++ 06_Lab_03/Weather-App/weather-app/weather-api.js
@@ -1,23 +1,27 @@
 
 
+
 // Define a constant for APP_ID
+const APP_ID = "fbc0ee13d4cd0ca7ed7b116299399f34";
 
 // Define a constant for Units
+const UNITS = "metric"
 
 // Define the baseURL as a constant
+const WEATHER_API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 class WeatherAPI {
 
   constructor(){
 
-    this.apiURL = new URL(/*baseURL*/)
+    this.apiURL = new URL(WEATHER_API_BASE_URL)
   }
   
   constructURL(locationName){
 
-    this.apiURL.searchParams.append("q", "");
-    // this.apiURL.searchParams.append("q", "");
-    
+    this.apiURL.searchParams.append("q", locationName);
+    this.apiURL.searchParams.append("appid", APP_ID);
+    this.apiURL.searchParams.append("units", UNITS);
   }
 
   invokeURL(){
