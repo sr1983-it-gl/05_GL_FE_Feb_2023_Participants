194726dd95f826b9db820b360016a7aa223b9cc2
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-10-08 11:15:56 +0530

    Weather App - Construct URL [Test] Implementation

--- 06_Lab_03/Weather-App/weather-app/weather-api-test.js
+++ 06_Lab_03/Weather-App/weather-app/weather-api-test.js
@@ -1,8 +1,16 @@
 
+import {WeatherAPI} from "./weather-api.js"
 
 function testConstructURL(){
 
   // object for weather-api
   // invoke the function
   // 
+
+  const weatherAPIObj = new WeatherAPI();
+  weatherAPIObj.constructURL("Brazil");
+
 }
+
+
+testConstructURL();
--- 06_Lab_03/Weather-App/weather-app/weather-api.js
+++ 06_Lab_03/Weather-App/weather-app/weather-api.js
@@ -28,3 +28,5 @@ class WeatherAPI {
 
   }
 }
+
+export {WeatherAPI}
