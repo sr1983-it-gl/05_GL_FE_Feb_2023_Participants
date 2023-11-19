965d37bd45796bff65b9248906b53296d9624acd
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-11-19 14:11:42 +0530

    Invoke getAllExpenseItems

--- 08_Lab_04/Expense-Tracker/client/src/components/ExpenseTrackerHome.tsx
+++ 08_Lab_04/Expense-Tracker/client/src/components/ExpenseTrackerHome.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react"
+import { getAllExpenseItems } from "../services/expense-service"
 
 
 const ExpenseTrackerHome = () => {
+
+  useEffect( () => {
+
+    const getAllExpenseItemsInvoker = () =>  {
+
+      // Call the function - getAllExpenseItems
+      // await / async
+      // log data
+      // verify - 5 expense items
+
+    }
+
+    getAllExpenseItemsInvoker();
+  })
 
   return (
     <div>
